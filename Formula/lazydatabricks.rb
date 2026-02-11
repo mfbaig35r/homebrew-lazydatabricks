1b702801e2f6ab8588b372c61c0e051ee2612ee5
fbaig <mfbaig35r@gmail.com>
2026-02-10 21:10:13 -0700

    Update homepage URL

--- Formula/lazydatabricks.rb
+++ Formula/lazydatabricks.rb
@@ -2,7 +2,7 @@ class Lazydatabricks < Formula
   include Language::Python::Virtualenv
 
   desc "A keyboard-first TUI for Databricks ops — lazygit for your data platform"
-  homepage "https://github.com/yourusername/lazybricks"
+  homepage "https://github.com/mfbaig35r/lazybricks"
   url "https://files.pythonhosted.org/packages/7e/cf/d7846974da6a41665a22f14be5e3558198ef42e533b2bcc5838a8613754d/lazydatabricks-1.0.0.tar.gz"
   sha256 "8409a088449b6613bf8a34ef9450b4e32f9bb65a6c7706a855650bcb77e01423"
   license "MIT"
